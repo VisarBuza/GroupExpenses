9d7a5cfbf96c09e0782fa2eedb3f238578d478e7
Mariusz Nowostawski <mariusz.nowostawski@ntnu.no>
2020-02-09 15:10:11 +0100

    Better formatting.

--- app/src/main/java/imt3673/ass/groupexpenses/SingleExpense.kt
+++ app/src/main/java/imt3673/ass/groupexpenses/SingleExpense.kt
@@ -8,6 +8,6 @@ class SingleExpense(val person: String,
                     var description: String = "") {
 
     override fun toString(): String {
-        return "Expense{{$person}, {$amount}}"
+        return "Expense{$person, $amount}"
     }
 }
